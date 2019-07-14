7a67ca12ee789370559f31fa27d568d45af038d9
romans <roman233233@gmail.com>
2019-07-14 22:18:53 +0100

    backend challenge

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const portfinder = require('portfinder');
+const Redis = require('ioredis');
+const { scaleLinear } = require('d3-scale');
+
+// init express
+const app = express();
+
+// init redis; how to use: https://github.com/luin/ioredis#basic-usage
+const redis = new Redis();
+
+/**
+ * Use D3 to map values to the (x, y) position on the ASCII line chart.
+ * https://github.com/d3/d3-scale#linear-scales
+ * https://observablehq.com/@d3/d3-scalelinear
+ *
+ * @example
+ * const x = scaleLinear().domain([-100, 100]).range([0, 10]);
+ * x(35); // 6.75
+ * x(-80); // 1
+ */
+
+// do your magic here 👇
+app.get('/ascii', (req, res) => res.send('hello world'));
+
+// find an open port
+portfinder.getPort((err, port) => {
+  if (err) {
+    throw err;
+  }
+
+  // start the server
+  app.listen(port, () => console.log(`Running on :${port} 👍`));
+});
