771c30df81a96006b40674edc253a1d0af22e74c
Kylie <kyliepace@gmail.com>
2020-11-26 08:44:44 +0000

    make index.ts a self-invoked function because I only like code wrapped in functions :)

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,53 @@ import portfinder from 'portfinder';
 import Redis from 'ioredis';
 import { scaleLinear } from 'd3-scale';
 
-// init express
-const app = express();
+async function startServer() {
+  const app = express();
 
-// init redis; how to use: https://github.com/luin/ioredis#basic-usage
-const redis = new Redis();
+  /**
+   * A little hack here
+   * Import/Export can only be used in 'top-level code'
+   * Well, at least in node 10 without babel and at the time of writing
+   * So we are using good old require.
+   **/
+  await require('./loaders').default({ expressApp: app });
+
+  // find an open port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      throw err;
+    }
+
+    // start the server
+    app.listen(port, () => (
+      console.log(`Running on :${port} 👍`)
+    )).on('error', err => {
+      console.error(err);
+      process.exit(1);
+    });
+  });
+};
+
+startServer();
+
+
 
 /**
- * Use D3 to map values to the (x, y) position on the ASCII line chart.
- * https://github.com/d3/d3-scale#linear-scales
- * https://observablehq.com/@d3/d3-scalelinear
- *
- * @example
- * const x = scaleLinear().domain([-100, 100]).range([0, 10]);
- * x(35); // 6.75
- * x(-80); // 1
+ * code to move
  */
+// // init redis; how to use: https://github.com/luin/ioredis#basic-usage
+// const redis = new Redis();
 
-// do your magic here 👇
-app.get('/ascii', (req, res) => res.send('hello world'));
+// /**
+//  * Use D3 to map values to the (x, y) position on the ASCII line chart.
+//  * https://github.com/d3/d3-scale#linear-scales
+//  * https://observablehq.com/@d3/d3-scalelinear
+//  *
+//  * @example
+//  * const x = scaleLinear().domain([-100, 100]).range([0, 10]);
+//  * x(35); // 6.75
+//  * x(-80); // 1
+//  */
 
-// find an open port
-portfinder.getPort((err, port) => {
-  if (err) {
-    throw err;
-  }
-
-  // start the server
-  app.listen(port, () => console.log(`Running on :${port} 👍`));
-});
+// // do your magic here 👇
+// app.get('/ascii', (req, res) => res.send('hello world'));
